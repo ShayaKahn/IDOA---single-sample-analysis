a1ab003bed971089d5971099e1cb77251ba50c5b
ShayaKahn <102433115+ShayaKahn@users.noreply.github.com>
2023-05-04 10:33:13 +0300

    Update DOC.py

--- DOC.py
+++ DOC.py
@@ -11,18 +11,23 @@ class DOC:
         param cohort: a matrix, samples are in the rows.
         """
         self.cohort = cohort
-        self.num_samples = np.size(cohort, 0)
+        if self.cohort.ndim != 2:
+            raise ValueError("ref_cohort should be a 2D numpy array")
+        self.num_samples = cohort.shape[0]
 
     def calc_doc(self):
         """
         :return: matrix, the first row is the dissimilarity values for all the sample pairs of the
          cohort, the second row is for the overlap.
         """
+        # Calculate overlap values
         o = [[Overlap(self.cohort[j, :], self.cohort[i, :]).calculate_overlap() for i in range(
             j + 1, self.num_samples)] for j in range(0, self.num_samples - 1)]
+        # Calculate dissimilarity values
         d = [[Dissimilarity(self.cohort[j, :], self.cohort[i, :]).calculate_dissimilarity() for i in range(
-            j+1, self.num_samples)] for j in range(0, self.num_samples-1)]
+            j + 1, self.num_samples)] for j in range(0, self.num_samples - 1)]
 
+        # Organize the structure
         def flatten(lis):
             return [item for sublist in lis for item in sublist]
 
@@ -33,6 +38,17 @@ class DOC:
         return doc_mat
 
     def bootstrap(self):
+        """
+        This method applies bootstrap procedure, using leave one out, it calculates the DOC matrix for all the samples
+        excluding single sample each time.
+        :return: a container with all the DOC matrices for the procedure.
+        """
+        bootstrap_mat_container = []
+        for i in range(0, self.num_samples):
+            resampled_cohort = np.delete(self.cohort, i, axis=0)  # Remove the ith sample
+            resampled_doc = DOC(resampled_cohort)  # Initiate DOC
+            bootstrap_mat_container.append(resampled_doc.calc_doc())  # Apply DOC
+        return bootstrap_mat_container
         """
         :return: List of n DOC matrices after leave one out bootstrap is applied.
         """
